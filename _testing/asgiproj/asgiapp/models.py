33084a2059d08efb0f364764892c860d6e04512c
erm <jermff@gmail.com>
2018-06-10 17:27:00 +1000

    Removing test app from main repo

--- _testing/asgiproj/asgiapp/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-class AsyncTest(models.Model):
-
-    myfield = models.CharField(max_length=255)
--- _testing/asgiproj/asgiproj/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from asgiapp.views import TestView
-
-urlpatterns = [
-    path("test/", TestView.as_view()),
-    path("test/form/", TestView.as_view())
-]
